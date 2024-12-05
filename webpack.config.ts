6a83824e192fff029d9009646f9f0b7b47238892
m.kulik <camelot1399@gmail.com>
2024-12-05 13:36:22 +0300

    added webpack+css

--- webpack.config.ts
+++ webpack.config.ts
@@ -38,6 +38,10 @@ export default (env: EnvVariables) => {
     module: {
       rules: [
         {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
           test: /\.tsx?$/,
           use: "ts-loader",
           exclude: /node_modules/,
