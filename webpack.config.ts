0c29cd0d08b5193e61e27ef9ffd12a40fbc930fc
m.kulik <camelot1399@gmail.com>
2024-12-05 17:42:04 +0300

    refactored webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,74 +1,25 @@
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-
-type Mode = "production" | "development";
+import { buildWebpack } from "./config/build/buildWebpack";
+import { BuildMode, BuildPaths } from "./config/build/types/types";
+import path from "path";
 
 interface EnvVariables {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
 }
 
-export default (env: EnvVariables) => {
-  const isDev = env.mode === "development";
-  const isProd = env.mode === "production";
-
-  const devServerConfig = {
-    // static: {
-    //   directory: path.join(__dirname, "public"),
-    // },
-    port: env.port,
-    open: true,
-  };
-
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
+export default (env: EnvVariables): webpack.Configuration => {
+  const paths: BuildPaths = {
+    output: path.resolve(__dirname, "build"),
     entry: path.resolve(__dirname, "src", "index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }),
-      isDev && new webpack.ProgressPlugin(),
-      isProd &&
-        new MiniCssExtractPlugin({
-          filename: "css/[name].[contenthash:8].css",
-          chunkFilename: "css/[name].[contenthash:8].css",
-        }),
-    ].filter(Boolean),
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devtool: isDev && "inline-source-map",
-    devServer: isDev ? devServerConfig : undefined,
+    html: path.resolve(__dirname, "public", "index.html"),
   };
+
+  const config = buildWebpack({
+    port: env.port ?? 3000,
+    mode: env.mode ?? "development",
+    paths,
+  });
 
   return config;
 };
