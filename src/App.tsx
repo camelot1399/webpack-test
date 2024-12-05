fad48742ece1f9c8d24bd5f029fa7c14be7e1332
m.kulik <camelot1399@gmail.com>
2024-12-05 13:26:30 +0300

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>webpack test</h1>
+
+      <div>count: {count}</div>
+
+      <button onClick={() => setCount((count) => count + 1)}>click</button>
+    </>
+  );
 };
 
 export default App;
